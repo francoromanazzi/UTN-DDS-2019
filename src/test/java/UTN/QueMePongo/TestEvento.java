903a51bec04181857d6a23a8d19145249e6caeeb
christianinacio <christianginacio@gmail.com>
2019-05-25 18:02:58 -0300

    Agrego TestEvento

---
+++ src/test/java/UTN/QueMePongo/TestEvento.java
@@ -0,0 +1,36 @@
+package UTN.QueMePongo;
+
+import excepciones.FechaFinDebeSerAntesQueFechaInicioException;
+import excepciones.parametros_nulos.FechaFinNoPuedeSerNulaException;
+import excepciones.parametros_nulos.FechaInicioNoPuedeSerNulaException;
+import modelo.evento.EventoAlAireLibre;
+import org.junit.Before;
+import org.junit.Test;
+import java.time.LocalDateTime;
+import java.time.Month;
+
+public class TestEvento {
+    private LocalDateTime inicioFiesta;
+    private LocalDateTime finFiesta;
+
+    @Before
+    public void crearFechas(){
+        inicioFiesta = LocalDateTime.of(2019, Month.MAY, 25, 10, 0, 0);
+        finFiesta = LocalDateTime.of(2019, Month.MAY, 25, 15, 0, 0);
+    }
+
+    @Test(expected = FechaFinDebeSerAntesQueFechaInicioException.class)
+    public void fechaInicialDespuesDeLaFechaFin(){
+        new EventoAlAireLibre(finFiesta,inicioFiesta);
+    }
+
+    @Test(expected = FechaInicioNoPuedeSerNulaException.class)
+    public void fechaInicialNula(){
+        new EventoAlAireLibre(null, finFiesta);
+    }
+
+    @Test(expected = FechaFinNoPuedeSerNulaException.class)
+    public void fechaFinNula(){
+        new EventoAlAireLibre(inicioFiesta, null);
+    }
+}
