f0fb2a999604d25b5250403931a35aa8f6ab0df5
cimimuxmaio <cimimuxmaio2@gmail.com>
2019-09-05 20:41:20 -0300

    me equivoqué en algo respecto al commit anterior

--- src/main/java/modelo/usuario/Usuario.java
+++ src/main/java/modelo/usuario/Usuario.java
@@ -144,7 +144,7 @@ public class Usuario {
 	public void eliminarEvento(Evento e) {
 		eventos.remove(e);
 		// Hay un bug? Falta sacar del timer la generacion de sugerencias
-		// Habria que, de alguna forma, obtener el TimeTask correspondiente (new GenerarSugerencias(...)) y cancelarlo (cancel()).
+		// Habria que, de alguna forma, obtener el TimeTask correspondiente y cancelarlo (cancel()).
 	}
 	
 	public List<Sugerencia> obtenerSugerencias(Evento evento) throws EventoNoFueAgendadoException, EventoNoEstaProximoException, SinSugerenciasPosiblesException, PronosticoNoDisponibleException {
