7b47d3e16b45608b2792d4646131a79268e22c01
cimimuxmaio <cimimuxmaio2@gmail.com>
2019-11-25 14:28:30 -0300

    Refactor script de deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env bash
 
-mv src/main/resources/META-INF/persistence.xml src/main/resources/META-INF/persistence-local.xml
-mv src/main/resources/META-INF/persistence-deploy.xml src/main/resources/META-INF/persistence.xml
-mvn clean heroku:deploy -Dmaven.test.skip=true
-mv src/main/resources/META-INF/persistence.xml src/main/resources/META-INF/persistence-deploy.xml
-mv src/main/resources/META-INF/persistence-local.xml src/main/resources/META-INF/persistence.xml
+if mv src/main/resources/META-INF/persistence.xml src/main/resources/META-INF/persistence-local.xml;
+then
+    if mv src/main/resources/META-INF/persistence-deploy.xml src/main/resources/META-INF/persistence.xml;
+    then
+        if mvn clean heroku:deploy -Dmaven.test.skip=true;
+        then
+            echo "SUCCESS"
+        else
+            echo "ERROR IN DEPLOY"
+        fi
+
+        mv src/main/resources/META-INF/persistence.xml src/main/resources/META-INF/persistence-deploy.xml
+    else
+        echo "Error al renombrar persistence-deploy.xml a persistence.xml"
+    fi
+
+    mv src/main/resources/META-INF/persistence-local.xml src/main/resources/META-INF/persistence.xml
+else
+    echo "Error al renombrar persistence.xml a persistence.xml"
+fi
 
 read x
