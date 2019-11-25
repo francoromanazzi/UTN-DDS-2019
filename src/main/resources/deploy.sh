02ab0cb5df66555c7a0337631a0a260617cb3011
cimimuxmaio <cimimuxmaio2@gmail.com>
2019-11-25 13:45:53 -0300

    Agrego un input al bash para que no se ciere y poder leer lo que tira

--- src/main/resources/deploy.sh
+++ src/main/resources/deploy.sh
@@ -5,3 +5,5 @@ mv src/main/resources/META-INF/persistence-deploy.xml src/main/resources/META-IN
 mvn clean heroku:deploy -Dmaven.test.skip=true
 mv src/main/resources/META-INF/persistence.xml src/main/resources/META-INF/persistence-deploy.xml
 mv src/main/resources/META-INF/persistence-local.xml src/main/resources/META-INF/persistence.xml
+
+read x
